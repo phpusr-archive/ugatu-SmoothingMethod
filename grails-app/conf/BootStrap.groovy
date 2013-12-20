86eb2c8df7497fe61db674c1bdc006b3eb1d359b
phpusr <phpusr@gmail.com>
2013-12-20 13:44:02 +0600

    add: Пример задачи

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -13,9 +13,18 @@ class BootStrap {
 
         if (ret == 'nothing') return
 
-        def task1 = new Task(name: 'Test').save(failOnError: true)
-
-        new TaskData(task: task1, title: 'Title', value: 100500).save(failOnError: true)
+        //Задача: Уровень безработицы
+        def task1 = new Task(name: 'Уровень безработицы').save(failOnError: true)
+        new TaskData(task: task1, title: 'Январь', value: 2.99).save(failOnError: true)
+        new TaskData(task: task1, title: 'Февраль', value: 2.66).save(failOnError: true)
+        new TaskData(task: task1, title: 'Март', value: 2.63).save(failOnError: true)
+        new TaskData(task: task1, title: 'Апрель', value: 2.56).save(failOnError: true)
+        new TaskData(task: task1, title: 'Май', value: 2.40).save(failOnError: true)
+        new TaskData(task: task1, title: 'Июнь', value: 2.22).save(failOnError: true)
+        new TaskData(task: task1, title: 'Июль', value: 1.97).save(failOnError: true)
+        new TaskData(task: task1, title: 'Август', value: 1.72).save(failOnError: true)
+        new TaskData(task: task1, title: 'Сентябрь', value: 1.56).save(failOnError: true)
+        new TaskData(task: task1, title: 'Октябрь', value: 1.42).save(failOnError: true)
 
     }
     def destroy = {
