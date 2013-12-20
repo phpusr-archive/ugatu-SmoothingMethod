8409039b8a02245121e03e37a372d923932716bd
phpusr <phpusr@gmail.com>
2013-12-20 20:44:16 +0600

    improve: TaskData

--- grails-app/domain/smoothingmethod/method/TaskData.groovy
+++ grails-app/domain/smoothingmethod/method/TaskData.groovy
@@ -6,10 +6,16 @@ package smoothingmethod.method
 class TaskData {
 
     static constraints = {
+        task()
+        title blank: false
+        value()
     }
 
+    /** Задача */
     Task task
+    /** Заголовок */
     String title
+    /** Значение */
     Double value
 
 }
