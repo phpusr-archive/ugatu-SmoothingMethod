944fb5e3b2f6143602204d47add77c940eca593c
phpusr <phpusr@gmail.com>
2013-12-20 14:54:48 +0600

    add: Подключил Angular к проекту

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,14 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+
+    angularApp {
+        dependsOn 'angular', 'angular-route'
+
+        resource url:'js/angular-app/app.js'
+//        resource url:'js/angular-app/services.js'
+//        resource url:'js/angular-app/controllers.js'
+//        resource url:'js/angular-app/filters.js'
+//        resource url:'js/angular-app/directives.js'
+    }
 }
