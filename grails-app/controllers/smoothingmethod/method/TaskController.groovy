41b50b0afc7ce8decb436d72aba1fdc16004bc10
phpusr <phpusr@gmail.com>
2013-12-21 21:11:42 +0600

    add: Create & save actions

--- grails-app/controllers/smoothingmethod/method/TaskController.groovy
+++ grails-app/controllers/smoothingmethod/method/TaskController.groovy
@@ -1,8 +1,14 @@
 package smoothingmethod.method
 
 import grails.converters.JSON
+import grails.transaction.Transactional
+
+import static org.springframework.http.HttpStatus.CREATED
+import static org.springframework.http.HttpStatus.NOT_FOUND
 
 class TaskController {
+
+    static allowedMethods = [list: 'GET', save: 'POST']
 
     def index() {
         redirect(action: 'list', params: params)
@@ -22,10 +28,50 @@ class TaskController {
             html task: task
             json {
                 SmoothingMethodDataWrapper dataWrapper = new SmoothingMethod(task, a).calc()
-                //JSON.use('deep')
                 render dataWrapper as JSON
             }
         }
     }
 
+    def create() {
+        respond new Task(params)
+    }
+
+    @Transactional
+    def save() {
+        Task taskInstance
+        def jsonObject = request.JSON;
+        println 'jsonObject: ' + jsonObject
+        return //TODO
+
+        if (taskInstance == null) {
+            notFound()
+            return
+        }
+
+        if (taskInstance.hasErrors()) {
+            respond taskInstance.errors, view:'create'
+            return
+        }
+
+        taskInstance.save flush:true
+
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'taskInstance.label', default: 'Task'), taskInstance.id])
+                redirect taskInstance
+            }
+            '*' { respond taskInstance, [status: CREATED] }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'taskInstance.label', default: 'Task'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
 }
